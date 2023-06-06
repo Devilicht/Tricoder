1d02bde85cc484025e598ed390a9a024fc1ee537
oblivion <oblivionccyber@gmail.com>
2023-06-06 18:12:01 -0300

    ports scan created

---
+++ src/ports.rs
@@ -0,0 +1,34 @@
+use crate::{
+    commom_ports::MOST_COMMOM_PORTS10,
+    model::{Port, Subdomain},
+};
+use rayon::prelude::*;
+use std::net::{SocketAddr, ToSocketAddrs};
+use std::{net::TcpStream, time::Duration};
+
+pub fn scan_ports(mut subdomain: Subdomain) -> Subdomain{
+    let socket_addresses: Vec<SocketAddr> = format!("{}:1024",Subdomain.domain)
+        .to_socket_addrs()
+        .expect("port scanner: Creating socket address")
+        .collect();
+
+    if socket_addresses.is_empty(){
+        return subdomain;
+    }
+
+    subdomain.open_ports = MOST_COMMOM_PORTS10
+        .into_par_iter()
+        .map(|port| scan_port(socket_addresses[0], *port))
+        .filter(|port| port.is_open) //filter closed ports
+        .collect();
+    subdomain
+}
+
+fn scan_port(mut socket_addresses: SocketAddr, port: u16) -> Port{
+    let timeout = Duration::from_secs(3);
+    socket_address.set_port(port);
+
+    let is_open = TcpStream::connect_timeout(&socket_address, timeout).is_ok();
+
+    Port { port, is_open }
+}
